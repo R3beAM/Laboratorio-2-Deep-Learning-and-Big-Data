f454c0ec48845723b8d9346a9acc70527b2186ce
R3beAM <ralvarezmon@ucenfotec.ac.cr>
2025-08-12 09:36:09 -0600

    Update nn_pytorch_experiments.py

--- nn_pytorch_experiments.py
+++ nn_pytorch_experiments.py
@@ -5,13 +5,14 @@ from torch import nn
 from torch.utils.data import DataLoader, TensorDataset
 
 
-def load_data():
-    """Load and standardize the California housing dataset."""
-    data_path = 'CaliforniaHousing/cal_housing.data'
-    data = np.loadtxt(data_path, delimiter=',')
+def load_and_preprocess(path: str):
+    """Cargar datos, dividirlos en entrenamiento/prueba y estandarizar."""
+    # 1) Carga de los datos desde el archivo .data
+    data = np.loadtxt(path, delimiter=',')
     X = data[:, :-1]
     y = data[:, -1:]
 
+    # 2) División 80/20 utilizando una permutación fija para reproducibilidad
     rng = np.random.default_rng(42)
     indices = rng.permutation(len(X))
     split = int(len(X) * 0.8)
@@ -19,6 +20,7 @@ def load_data():
     X_train, X_test = X[train_idx], X[test_idx]
     y_train, y_test = y[train_idx], y[test_idx]
 
+    # 3) Estandarización de características y objetivo
     X_mean, X_std = X_train.mean(axis=0), X_train.std(axis=0)
     y_mean, y_std = y_train.mean(axis=0), y_train.std(axis=0)
     X_train = (X_train - X_mean) / X_std
@@ -26,13 +28,13 @@ def load_data():
     y_train = (y_train - y_mean) / y_std
     y_test = (y_test - y_mean) / y_std
 
+    # 4) Conversión a tensores y creación del DataLoader
     tensor_X_train = torch.tensor(X_train, dtype=torch.float32)
     tensor_y_train = torch.tensor(y_train, dtype=torch.float32)
     tensor_X_test = torch.tensor(X_test, dtype=torch.float32)
     tensor_y_test = torch.tensor(y_test, dtype=torch.float32)
-
     train_dataset = TensorDataset(tensor_X_train, tensor_y_train)
-    test_tensors = (tensor_X_test, tensor_y_test)
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 
     return train_dataset, test_tensors, y_mean, y_std
 
